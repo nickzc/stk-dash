2a3b087438db485997b45677c087f3f91dbe1b2c
zc <apple@zc-air.local>
2025-04-25 17:13:19 +1200

    Replace the symbols with ten different companies

--- stk-dash-backend/src/stocks/constants/stocks.constants.ts
+++ stk-dash-backend/src/stocks/constants/stocks.constants.ts
@@ -1,22 +1,15 @@
-// export const DEFAULT_STOCKS = [
-//   { symbol: 'AAPL', name: 'Apple Inc.' },
-//   { symbol: 'MSFT', name: 'Microsoft Corp.' },
-//   { symbol: 'AMZN', name: 'Amazon.com Inc.' },
-//   { symbol: 'GOOGL', name: 'Alphabet Inc.-C' },
-//   { symbol: 'META', name: 'Meta Platforms Inc.' },
-//   { symbol: 'TSLA', name: 'Tesla Inc.' },
-//   { symbol: 'BRK.B', name: 'Berkshire Hathaway B' },
-//   { symbol: 'BABA', name: 'Alibaba Group' },
-//   { symbol: 'ORCL', name: 'Oracle Corp.' },
-//   { symbol: 'ADBE', name: 'Adobe Inc.' },
-// ];
 //Mock a stock list for now; if time allows later, we can extend it to accept multiple stock symbols and generate the list dynamically.
 export const DEFAULT_STOCKS = [
   { symbol: 'AAPL', name: 'Apple Inc.' },
-  { symbol: 'AAPL', name: 'Apple Inc.' },
-  { symbol: 'AAPL', name: 'Apple Inc.' },
-  { symbol: 'AAPL', name: 'Apple Inc.' },
-  { symbol: 'AAPL', name: 'Apple Inc.' },
+  { symbol: 'MSFT', name: 'Microsoft Corp.' },
+  { symbol: 'AMZN', name: 'Amazon.com Inc.' },
+  { symbol: 'GOOGL', name: 'Alphabet Inc.-C' },
+  { symbol: 'META', name: 'Meta Platforms Inc.' },
+  { symbol: 'TSLA', name: 'Tesla Inc.' },
+  { symbol: 'BRK.B', name: 'Berkshire Hathaway B' },
+  { symbol: 'BABA', name: 'Alibaba Group' },
+  { symbol: 'ORCL', name: 'Oracle Corp.' },
+  { symbol: 'ADBE', name: 'Adobe Inc.' },
 ];
 
 // Alpha Vantage API Key
